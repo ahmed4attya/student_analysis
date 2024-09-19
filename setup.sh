1dc6b318945504859efade98f302323d33d9dd21
ahmed4attya <40062081+ahmed4attya@users.noreply.github.com>
2024-09-19 10:26:16 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# تنزيل وتثبيت wkhtmltopdf
+apt-get update
+apt-get install -y wget
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
+apt-get install -f
